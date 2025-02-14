bbe7a771d5fde146ce1378e3ce81e5eaf38c05e6
Benedict Thompson <git@benjft.uk>
2025-02-14 01:22:21 +0000

    Refactor service attribute tests into focused test classes

---
+++ Benjft.Util.DependencyInjection.Tests/Attributes/ServiceAttribute/FactoryMethodTests.cs
@@ -0,0 +1,146 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Benjft.Util.DependencyInjection.Attributes;
+using Benjft.Util.DependencyInjection.Exceptions;
+using Benjft.Util.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Benjft.Util.DependencyInjection.Tests.Attributes;
+
+public class FactoryMethodTests {
+    
+    [Fact]
+    public void ServiceAttribute_AddsService_WhenUsedOnClass() {
+        var serviceType = typeof(ServiceFactoryAttributeExample);
+        var factoryMethod = GetFactoryMethod(serviceType, nameof(ServiceFactoryAttributeExample.Create));
+
+        var services = serviceType.GetServicesFromAttributes();
+        
+        var actualService = Assert.Single(services);
+        Assert.Equal(serviceType, actualService.ServiceType);
+        Assert.Equal(factoryMethod, actualService.ImplementationFactory);
+    }
+    
+    [Fact]
+    public void ServiceAttribute_AddsService_WhenUsedOnAbstractClassIfFactoryMethodExists() {
+        var serviceType = typeof(AbstractServiceFactoryAttributeExample);
+        var factoryMethod = GetFactoryMethod(serviceType, nameof(AbstractServiceFactoryAttributeExample.Create));
+
+        var services = serviceType.GetServicesFromAttributes();
+        
+        var actualService = Assert.Single(services);
+        Assert.Equal(serviceType, actualService.ServiceType);
+        Assert.Equal(factoryMethod, actualService.ImplementationFactory);
+    }
+    
+    [Theory]
+    [InlineData(ServiceLifetime.Singleton)]
+    [InlineData(ServiceLifetime.Scoped)]
+    [InlineData(ServiceLifetime.Transient)]
+    public void ServiceAttribute_UsesDefaultLifetime_WhenNoLifetimeIsSet(ServiceLifetime defaultLifetime) {
+        var serviceType = typeof(ServiceFactoryAttributeExample);
+        var factoryMethod = GetFactoryMethod(serviceType, nameof(ServiceFactoryAttributeExample.Create));
+        
+        var services = serviceType.GetServicesFromAttributes(defaultLifetime);
+        
+        var actualService = Assert.Single(services);
+        Assert.Equal(actualService.Lifetime, defaultLifetime);
+        Assert.Equal(factoryMethod, actualService.ImplementationFactory);
+    }
+    
+    [Theory]
+    [InlineData(typeof(TransientServiceFactoryAttributeExample), ServiceLifetime.Transient)]
+    [InlineData(typeof(ScopedServiceFactoryAttributeExample), ServiceLifetime.Scoped)]
+    [InlineData(typeof(SingletonServiceFactoryAttributeExample), ServiceLifetime.Singleton)]
+    public void ServiceAttribute_UsesLifetimeSetInTheAttribute_WhenLifetimeIsSet(
+        Type serviceType,
+        ServiceLifetime expectedLifetime) {
+        var defaultLifetime = expectedLifetime != ServiceLifetime.Singleton ? ServiceLifetime.Singleton : ServiceLifetime.Transient;
+        
+        var services = serviceType.GetServicesFromAttributes(defaultLifetime);
+        
+        var factoryMethod = GetFactoryMethod(serviceType, "Create");
+        var actualService = Assert.Single(services);
+        Assert.Equal(actualService.Lifetime, expectedLifetime);
+        Assert.Equal(factoryMethod, actualService.ImplementationFactory);
+    }
+
+    [Theory]
+    [InlineData(typeof(ServiceFactoryAttributeExampleIncorrectSignature),
+                typeof(InvalidFactoryMethodException),
+                typeof(FactoryMethodHasWrongSignatureException))]
+    [InlineData(typeof(ServiceFactoryAttributeExampleFactoryDoesNotExist),
+                typeof(FactoryMethodNotFoundException),
+                null)]
+    [InlineData(typeof(ServiceFactoryAttributeExampleFactoryNotStatic),
+                typeof(InvalidFactoryMethodException),
+                typeof(FactoryMethodNotStaticException))]
+    [InlineData(typeof(ServiceFactoryAttributeExampleWrongReturnType),
+                typeof(InvalidServiceTypeException),
+                null)]
+    public void ServiceAttribute_ShouldThrowAnException_WhenFactoryMethodIsInvalid(Type serviceType, Type exceptionType, Type? innerExceptionType) {
+        var actualException = Assert.Throws(exceptionType, () => serviceType.GetServicesFromAttributes().ToList());
+        
+        if (innerExceptionType != null) {
+            Assert.IsType(innerExceptionType, actualException.InnerException);
+        }
+    }
+
+    private static Func<IServiceProvider, object> GetFactoryMethod(Type serviceType, string factoryMethodName) {
+        return serviceType.GetMethod(factoryMethodName)!.CreateDelegate<Func<IServiceProvider, object>>();
+    }
+
+    #region Test Classes
+    
+    [Service(FactoryMethod = nameof(Create))]
+    private class ServiceFactoryAttributeExample {
+        [ExcludeFromCodeCoverage]
+        public static ServiceFactoryAttributeExample Create(IServiceProvider serviceProvider) => new();
+    }
+
+    [Service(FactoryMethod = nameof(Create))]
+    private abstract class AbstractServiceFactoryAttributeExample {
+        [ExcludeFromCodeCoverage]
+        public static AbstractServiceFactoryAttributeExample Create(IServiceProvider serviceProvider) => throw new NotImplementedException();
+    }
+
+    [TransientService(FactoryMethod = nameof(Create))]
+    private class TransientServiceFactoryAttributeExample {
+        [ExcludeFromCodeCoverage]
+        public static TransientServiceFactoryAttributeExample Create(IServiceProvider serviceProvider) => new();
+    }
+
+    [ScopedService(FactoryMethod = nameof(Create))]
+    private class ScopedServiceFactoryAttributeExample {
+        [ExcludeFromCodeCoverage]
+        public static ScopedServiceFactoryAttributeExample Create(IServiceProvider serviceProvider) => new();
+    }
+
+    [SingletonService(FactoryMethod = nameof(Create))]
+    private class SingletonServiceFactoryAttributeExample {
+        [ExcludeFromCodeCoverage]
+        public static SingletonServiceFactoryAttributeExample Create(IServiceProvider serviceProvider) => new();
+    }
+    
+    [Service(FactoryMethod = nameof(Create))]
+    private class ServiceFactoryAttributeExampleWrongReturnType {
+        [ExcludeFromCodeCoverage]
+        public static ServiceFactoryAttributeExample Create(IServiceProvider serviceProvider) => new();
+    }
+
+    [Service(FactoryMethod = "Create")]
+    private class ServiceFactoryAttributeExampleFactoryDoesNotExist;
+    
+    [Service(FactoryMethod = nameof(Create))]
+    private class ServiceFactoryAttributeExampleFactoryNotStatic {
+        [ExcludeFromCodeCoverage]
+        public ServiceFactoryAttributeExampleFactoryNotStatic Create(IServiceProvider serviceProvider) => new();
+    }
+    
+    [Service(FactoryMethod = nameof(Create))]
+    private class ServiceFactoryAttributeExampleIncorrectSignature {
+        [ExcludeFromCodeCoverage]
+        public static ServiceFactoryAttributeExampleIncorrectSignature Create() => new();
+    }
+    
+    #endregion Test Classes
+}
---
+++ Benjft.Util.DependencyInjection.Tests/Attributes/ServiceAttribute/KeyedServiceTests.cs
@@ -0,0 +1,115 @@
+﻿using Benjft.Util.DependencyInjection.Attributes;
+using Benjft.Util.DependencyInjection.Exceptions;
+using Benjft.Util.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Benjft.Util.DependencyInjection.Tests.Attributes;
+
+public class KeyedServiceTests {
+    [Fact]
+    public void KeyedServiceAttribute_CreatesKeyedServices_WhenUsedOnClass() {
+        var serviceType = typeof(KeyedServiceAttributeExample);
+        
+        var services = serviceType.GetServicesFromAttributes();
+        
+        var actualService = Assert.Single(services);
+        Assert.True(actualService.IsKeyedService);
+        Assert.Equal(ServiceKeyEnum.Key1, actualService.ServiceKey);
+    }
+    
+    [Fact]
+    public void KeyedServiceAttribute_ThrowsException_WhenUsedOnAbstractClass() {
+        var serviceType = typeof(AbstractKeyedServiceAttributeExample);
+
+        Assert.Throws<InvalidServiceTypeException>(() => serviceType.GetServicesFromAttributes().ToList());
+    }
+    
+    [Theory]
+    [InlineData(ServiceLifetime.Singleton)]
+    [InlineData(ServiceLifetime.Scoped)]
+    [InlineData(ServiceLifetime.Transient)]
+    public void KeyedServiceAttribute_UsesDefaultLifetime_WhenNoLifetimeIsSet(ServiceLifetime defaultLifetime) {
+        var serviceType = typeof(KeyedServiceAttributeExample);
+        
+        var services = serviceType.GetServicesFromAttributes(defaultLifetime);
+        
+        var actualService = Assert.Single(services);
+        Assert.Equal(actualService.Lifetime, defaultLifetime);
+        Assert.Equal(ServiceKeyEnum.Key1, actualService.ServiceKey);
+    }
+    
+    [Theory]
+    [InlineData(typeof(TransientKeyedServiceAttributeExample), ServiceLifetime.Transient)]
+    [InlineData(typeof(ScopedKeyedServiceAttributeExample), ServiceLifetime.Scoped)]
+    [InlineData(typeof(SingletonKeyedServiceAttributeExample), ServiceLifetime.Singleton)]
+    public void KeyedServiceAttribute_UsesLifetimeSetInTheAttribute_WhenLifetimeIsSet(
+        Type serviceType,
+        ServiceLifetime expectedLifetime) {
+        var defaultLifetime = expectedLifetime != ServiceLifetime.Singleton ? ServiceLifetime.Singleton : ServiceLifetime.Transient;
+        
+        var services = serviceType.GetServicesFromAttributes(defaultLifetime);
+        
+        var actualService = Assert.Single(services);
+        Assert.Equal(actualService.Lifetime, expectedLifetime);
+        Assert.Equal(ServiceKeyEnum.Key1, actualService.ServiceKey);
+    }
+
+    [Fact]
+    public void KeyedServiceAttribute_CreatesMultipleKeyedServices_WhenMultipleKeysAreSet() {
+        var serviceType = typeof(KeyedServiceAttributeMultipleExample);
+
+        var services = serviceType.GetServicesFromAttributes().ToArray();
+        
+        Assert.Equal(3, services.Length);
+        Assert.Contains(ServiceKeyEnum.Key1, services.Select(s => s.ServiceKey));
+        Assert.Contains(ServiceKeyEnum.Key2, services.Select(s => s.ServiceKey));
+        Assert.Contains(ServiceKeyEnum.Key3, services.Select(s => s.ServiceKey));
+    }
+
+    [Fact]
+    public void KeyedServiceAttribute_AddsServicesInOrder_WhenOrderIsSpecified() {
+        var serviceType = typeof(KeyedServiceAttributeMultipleOrderedExample);
+
+        var services = serviceType.GetServicesFromAttributes().ToArray();
+        
+        Assert.Equal(3, services.Length);
+        Assert.Equal(ServiceKeyEnum.Key2, services[0].ServiceKey);
+        Assert.Equal(ServiceKeyEnum.Key3, services[1].ServiceKey);
+        Assert.Equal(ServiceKeyEnum.Key1, services[2].ServiceKey);
+    }
+
+    #region Test Classes
+    
+    [Service(ServiceKey = ServiceKeyEnum.Key1)]
+    private class KeyedServiceAttributeExample;
+    
+    [Service(ServiceKey = ServiceKeyEnum.Key1)]
+    private abstract class AbstractKeyedServiceAttributeExample;
+
+    [TransientService(ServiceKey = ServiceKeyEnum.Key1)]
+    private class TransientKeyedServiceAttributeExample;
+
+    [ScopedService(ServiceKey = ServiceKeyEnum.Key1)]
+    private class ScopedKeyedServiceAttributeExample;
+
+    [SingletonService(ServiceKey = ServiceKeyEnum.Key1)]
+    private class SingletonKeyedServiceAttributeExample;
+
+    [Service(ServiceKey = ServiceKeyEnum.Key1)]
+    [Service(ServiceKey = ServiceKeyEnum.Key2)]
+    [Service(ServiceKey = ServiceKeyEnum.Key3)]
+    private class KeyedServiceAttributeMultipleExample;
+
+    [Service(ServiceKey = ServiceKeyEnum.Key1, Order = 3)]
+    [Service(ServiceKey = ServiceKeyEnum.Key2, Order = 1)]
+    [Service(ServiceKey = ServiceKeyEnum.Key3, Order = 2)]
+    private class KeyedServiceAttributeMultipleOrderedExample;
+
+    private enum ServiceKeyEnum {
+        Key1,
+        Key2,
+        Key3,
+    }
+    
+    #endregion Test Classes
+}
